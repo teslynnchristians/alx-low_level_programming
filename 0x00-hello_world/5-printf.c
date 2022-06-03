e59102d6d95ffd9fc3ed3b9782cbb030c4ec51d4
teslynnchristians <teslynnchr02@gmail.com>
2022-06-02 18:12:47 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints the size of various types
- * Return: 0 if exited properly. non-zero otherwise
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	  printf("with proper grammar, but the outcome is a piece of art,\n");
-
-	    return (0);
+	  return (0);
 }

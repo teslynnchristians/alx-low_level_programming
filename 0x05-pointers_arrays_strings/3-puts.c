92070b7d165ec4d4fab61689ea4b739c645db1a4
teslynnchristians <104847651+teslynnchristians@users.noreply.github.com>
2022-06-14 00:01:52 +0200

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,14 @@
+#iclude "main.h"
 
+/**
+ * _puts - this is a fuction that put
+ *@str: a param to _puts function
+ */
+void _puts(char *str)
+{
+for(; *str != '\o'; str++)
+{
+_putchar(*str);
+}
+_putchar('\n') 
+}   

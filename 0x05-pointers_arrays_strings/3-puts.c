2ed735fa5434a67c6ebfaad41163ea916bd563df
teslynnchristians <teslynnchr02@gmail.com>
2022-06-13 13:30:58 -0700

    I can only go one way. I've not got a reverse gear

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,8 +1,6 @@
 /**
  * _puts - prints out a string, followed by a new line
- *
  * @str: the string to be printed out
- *
  * Return: the string
  */
 void _puts(char *str)
@@ -11,7 +9,7 @@ int i = 0;
 
 while (str[i] != '\0')
 {
-_putchar(str[i]);
+_putchar(strrev[i]);
 i++;
 }
 _putchar('\n');

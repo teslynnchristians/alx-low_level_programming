64e4916699b3214ef16f590786313d3cab7bc1a5
teslynnchristians <teslynnchr02@gmail.com>
2022-06-14 06:24:14 -0700

    Don't swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *swap_int - given two intregersswap the values they are holging
+ *@a: parameter 1
+ *@b: parameter 2
+ */
+void swap_int(int *a, int *b)
+{
+int tmp;
+
+tmp = *a;
+*a = *b;
+*b = tmp;
+}	

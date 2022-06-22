68c29632f35873a4a00c6c3bc14a13005fc03f1b
teslynnchristians <teslynnchr02@gmail.com>
2022-06-22 05:58:26 -0700

    Inception. Now, before you bother telling me it's impossible...

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -35,25 +35,6 @@ return (len);
  */
 void iterate_wild(char **wildstr)
 {
-if (**wildstr == '*')
-{
-(*wildstr)++;
-iterate_wild(wildstr);
-}	
-}
-
-/**
- * postfix_match - Checks if a string str matches the postfix of
- *                 another string potentially containing wildcards.
- * @str: The string to be matched.
- * @postfix: The postfix.
- *
- * Return: If str and postfix are identical - a pointer to the null byte
- *                                            located at the end of postfix.
- *         Otherwise - a pointer to the first unmatched character in postfix.
- */
-char *postfix_match(char *str, char *postfix)
-{
 int str_len = strlen_no_wilds(str) - 1;
 int postfix_len = strlen_no_wilds(postfix) - 1;
 

9e9795282068feb19e0e8912d97a12493a4fbce7
teslynnchristians <teslynnchr02@gmail.com>
2022-06-24 15:56:02 -0700

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -2,18 +2,17 @@
 
 /**
  * _puts - function that prints a string, followed by a new line, to stdout
- *
  * @str: parameter defined in main
- *
  * Return: void
  */
-
 void _puts(char *str)
 {
-	while (*str != '\0')
-	{
-		_putchar(*str);
-		str++;
-	}
-	_putchar('\n');
+int i = 0;
+
+while (str[i])
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
 }

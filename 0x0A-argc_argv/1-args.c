c976ab2544d9fcd29210e2f6c93d4363afd3781f
teslynnchristians <teslynnchr02@gmail.com>
2022-06-26 03:37:48 -0700

    C - argc, argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int i;
+
+if (argc == 1)\
+printf("%d\n", argc - 1);
+else
+{
+for (i = 0; *argv; i++, argv++);
+
+printf("%d\n", i - 1);
+}
+
+return (0);
+}

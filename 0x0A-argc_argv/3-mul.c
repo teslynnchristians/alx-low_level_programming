db76923afcf7e552d5a6a8acbcc2d057082e3fb9
teslynnchristians <teslynnchr02@gmail.com>
2022-06-26 04:12:12 -0700

    Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 int i, j;
 
 if (argc < 3)
-{		
+{	
 printf("Error\n");
 return (1);
 }
@@ -25,7 +25,7 @@ for (i = 1; i < 3; i++)
 j *= atoi(argv[i]);
 
 printf("%d\n", j);
-}		
+}
 
 return (0);
-}	
+}

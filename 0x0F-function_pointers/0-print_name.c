2c9af92ef12a476dcb61656e18ac67f0c911b1fd
teslynnchristians <teslynnchr02@gmail.com>
2022-07-06 04:20:14 -0700

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints name by dereferncing the function f
+ *
+ * @name: para 1
+ * @f: function
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (f == NULL)
+{
+return;
+}
+(*f)(name);
+}

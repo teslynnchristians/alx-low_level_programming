c2f705b8a7230a83f6844713d7fdb4cc3bf290ad
teslynnchristians <teslynnchr02@gmail.com>
2022-07-06 04:16:16 -0700

    Most hackers are young because young people tend to be adaptable. As long as you remain adaptable, you can always be a good hacker

---
+++ 0x0F-function_pointers/main.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * is_98 - check if a number is equal to 98
+ * @elem: the integer to check
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+int is_98(int elem)
+{
+return (98 == elem);
+}
+
+/**
+ * is_strictly_positive - check if a number is greater than 0
+ * @elem: the integer to check
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+int is_strictly_positive(int elem)
+{
+ return (elem > 0);
+}
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (size <= 0)
+{
+/* code */
+return -1;
+}
+
+else if (array && cmp && size)
+{
+/* code */
+for (i = 0; i < size; i++)
+{
+/* code */
+int a = (cmp(*(array+i)));
+if (a == 1)
+{
+/* code */
+return (i);
+}
+
+
+
+}
+
+}
+return (-1);
+}
+/**
+ * abs_is_98 - check if the absolute value of a number is 98
+ * @elem: the integer to check
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+int abs_is_98(int elem)
+{
+return (elem == 98 || -elem == 98);
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
+int index;
+
+index = int_index(array, 20, is_98);
+printf("%d\n", index);
+index = int_index(array, 20, abs_is_98);
+printf("%d\n", index);
+index = int_index(array, 20, is_strictly_positive);
+printf("%d\n", index);
+return (0);
+}

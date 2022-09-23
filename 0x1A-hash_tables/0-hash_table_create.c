76c64a882ed1922112d682a990b8c47081afbc7b
teslynnchristians <teslynnchr02@gmail.com>
2022-09-23 01:46:31 -0700

    C hash tables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+i**
+ * hash_table_create - create a hash table
+ * @size: Table size
+ * Return: succes
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+hash_table_t *table;
+table = malloc(sizeof(hash_table_t));
+if (table == NULL)
+return (NULL);
+table->size = size;
+table->array = malloc(sizeof(table->array) * size);
+if (table->array == NULL)
+return (NULL);
+return (table);
+}
